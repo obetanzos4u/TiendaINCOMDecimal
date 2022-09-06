afe6610b3a3c94e0c7ec2a12c804ea90201832e3
omar <obetanzos@it4u.com.mx>
2022-09-06 11:46:14 -0500

    COM Catálogos to menu

--- userControls/menu_principal.ascx.cs
+++ userControls/menu_principal.ascx.cs
@@ -42,9 +42,26 @@ public partial class menuPrincipal : System.Web.UI.UserControl
         biblioteca.InnerHtml = "<a title='Biblioteca'>Biblioteca <img alt='Flecha de desplazamiento para ver menu' id='menu_icon_biblioteca' src='../img/webUI/newdesign/Flecha.svg'></a><ul><li><a href='https://blog.incom.mx/'>Blog</a></li><li><a href='https://www.incom.mx/ense%C3%B1anza/infograf%C3%ADas'>Infografías</a></li><li><a href='https://www.incom.mx/glosario/a'>Enciclopédico</a></li></ul>";
 
         HtmlGenericControl catalogos = new HtmlGenericControl("li");
-        catalogos.ID = "content-menu-incom-outlet";
+        catalogos.ID = "content-menu-incom-catalogos";
         catalogos.ClientIDMode = ClientIDMode.Static;
-        catalogos.InnerHtml = "<a title='Catalogos'  href='/'>Catálogos <img alt='Flecha de desplazamiento para ver menu' id='menu_icon_catalogos' src='../img/webUI/newdesign/Flecha.svg'></a>";
+        catalogos.InnerHtml = "<a title = 'Catalogos'  href = '/' >Catálogos<img alt = 'Flecha de desplazamiento para ver menu' id = 'menu_icon_catalogos' src = '../img/webUI/newdesign/Flecha.svg'></a>" +
+                "<ul>" +
+                    "<li><a href'/documents/pdf/CATALOGO_INCOM_FIBRA_OPTICA.pdf' target='_blank' title='Fibra Óptica'>Fibra óptica</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_AEREO.pdf' target='_blank' title='Aéreo'>Instalación aérea</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_SUBTERRANEO.pdf' target='_blank' title='Subterráneo'>Instalación subterránea</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_ESTRUCTURADO.pdf' target='_blank' title='Cableado estructurado'>Estructurado y data center</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_TELEFONIA_Y_CATV.pdf' target='_blank' title='Telefonía & CATV'>Telefonía y CATV</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_CANALIZACION.pdf' target='_blank' title='Canalización'>Canalización</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_IDENTIFICACION.pdf' target='_blank' title='Identificación'>Identificación de redes</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_ELECTRICO.pdf' target='_blank' title='Soluciones eléctricas'>Eléctrico</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_FERRETERIA.pdf' target='_blank' title='Ferretería'>Ferretería</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_INCOM_BLOQUEO.pdf' target='_blank' title='Bloqueo'>Seguridad industrial</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_2RENT.pdf' target='_blank' title='2Rent Venta y Renta'>Venta y renta'</a></li>" +
+                    "<li><a href='/documents/pdf/CATALOGO_MAQUINAS_PARA_SOPLADO_FREMCO.pdf' target='_blank' title='Fremco'>Sopladoras Fremco</a></li>" +
+                    "<li><a href='/documents/pdf/INCOM_CATALOGO_SOLUCION_FIBRA_SOPLADA.pdf' target='_blank' title='Solución Fibra Soplada'>Solución fibra soplada</a></li>" +
+                    "<li><a href='/documents/promos/CATALOGO_INCOM_LIQUIDACION.pdf' target='_blank' title='Liquidación'>Liquidación</a></li>" +
+                "</ul>";
+
         //  HtmlGenericControl blog = new HtmlGenericControl("li");
         //  blog.InnerHtml = "<a title='Blog Incom' target='_blank'  href='https://blog.incom.mx'>Blog</a>";
 
