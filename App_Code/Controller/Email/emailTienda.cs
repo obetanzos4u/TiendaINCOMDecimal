f813e3135067934a1de9f6cf92f0bba3f00e72db
Alexis Ruiz at INCOM <101216883+aruiz-incom@users.noreply.github.com>
2022-11-28 15:25:31 -0600

    FIX llaves de Santander productivo para ambos contextos.

--- App_Code/Controller/Email/emailTienda.cs
+++ App_Code/Controller/Email/emailTienda.cs
@@ -10,107 +10,127 @@ using System.Web;
 /// <summary>
 /// Descripción breve de email_Cotizaciones
 /// </summary>
-public class emailTienda : email {
+public class emailTienda : email
+{
 
-    
-    public emailTienda(string _asunto, string _destinatarios, string _mensaje, string _remitente) : base(_asunto, _destinatarios, _mensaje, _remitente) {
+
+    public emailTienda(string _asunto, string _destinatarios, string _mensaje, string _remitente) : base(_asunto, _destinatarios, _mensaje, _remitente)
+    {
         resultado = false;
         resultadoMensaje = "No se ha ejecutado el método de envío de email";
     }
 
-    private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e) {
+    private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+    {
 
         // StreamWriter fileWriter = new StreamWriter(appPath + @"/logErrores/listaCorreos.txt", true, System.Text.Encoding.UTF8);
 
-     //   Exception ex = (Exception)e.UserState;
+        //   Exception ex = (Exception)e.UserState;
 
-        if (e.Cancelled) {
+        if (e.Cancelled)
+        {
             //   fileWriter.Write(e.Cancelled);
-            }
-        if (e.Error != null) {
+        }
+        if (e.Error != null)
+        {
 
             devNotificaciones.error("Enviar Email Notificaciones", e.Error);
             //   fileWriter.Write("[{0}] {1}", ex.ToString(), e.Error.ToString());
 
-            } else {
-
-            }
-        // fileWriter.Close();
         }
+        else
+        {
 
-    public  void enviarRegistroBienvenida() {
+        }
+        // fileWriter.Close();
+    }
+
+    public void enviarRegistroBienvenida()
+    {
 
 
         mm = new MailMessage();
 
-        if (validarDestinatarios()) {
+        if (validarDestinatarios())
+        {
             depurarDestinatarios();
 
 
             mm.From = new MailAddress(remitenteCredenciales, "Incom Retail");
-                mm.Subject = asunto + " " + utilidad_fechas.obtenerCentral().ToString("f");
-                mm.IsBodyHtml = true;
-                mm.Body = mensaje;
-            mm.Bcc.Add("desarrollo@incom.mx");
-            mm.Bcc.Add("development@incom.mx");
-                mm.Bcc.Add("telemarketing@incom.mx");
-               mm.ReplyToList.Add("cmiranda@it4u.com.mx");
+            mm.Subject = asunto + " " + utilidad_fechas.obtenerCentral().ToString("f");
+            mm.IsBodyHtml = true;
+            mm.Body = mensaje;
+            //mm.Bcc.Add("desarrollo@incom.mx");
+            //mm.Bcc.Add("development@incom.mx");
+            mm.Bcc.Add("telemarketing@incom.mx");
+            mm.Bcc.Add("serviciosweb@incom.mx");
+            //mm.ReplyToList.Add("cmiranda@it4u.com.mx");
             SmtpClient enviar = smtp();
 
-              enviar.SendAsync(mm,null);
+            enviar.SendAsync(mm, null);
 
             resultado = true;
             resultadoMensaje = "Solicitud procesada";
-            } else {
+        }
+        else
+        {
 
             resultado = false;
             resultadoMensaje = "Destinatario(s) no válido(s)";
-            }
+        }
 
-       }
- 
-    public  void contacto() {
+    }
+
+    public void contacto()
+    {
 
 
         mm = new MailMessage();
 
-        if (validarDestinatarios()) {
+        if (validarDestinatarios())
+        {
             depurarDestinatarios();
 
- 
+
             mm.From = new MailAddress(remitenteCredenciales, "Incom Retail");
-            mm.Subject = "[Contacto] "+asunto + " " + utilidad_fechas.obtenerCentral().ToString("f");
+            mm.Subject = "[Contacto] " + asunto + " " + utilidad_fechas.obtenerCentral().ToString("f");
             mm.IsBodyHtml = true;
             mm.Body = mensaje;
-            mm.Bcc.Add("development@incom.mx");
+            //mm.Bcc.Add("development@incom.mx");
+            mm.Bcc.Add("serviciosweb@incom.mx");
             mm.Bcc.Add("fgarcia@incom.mx");
             mm.Bcc.Add("desarrollo@incom.mx");
             mm.Bcc.Add("telemarketing@incom.mx");
+            mm.Bcc.Add("jaraujo@incom.mx");
 
             SmtpClient enviar = smtp();
             enviar.SendAsync(mm, null);
 
             resultado = true;
             resultadoMensaje = "Solicitud procesada";
-            } else {
+        }
+        else
+        {
             resultado = false;
             resultadoMensaje = "Destinatario(s) no válido(s)";
-            }
-
         }
-    public void general() {
+
+    }
+    public void general()
+    {
         mm = new MailMessage();
 
-        if (validarDestinatarios()) {
+        if (validarDestinatarios())
+        {
             depurarDestinatarios();
 
-           // mm.Headers.Add("", "");
+            // mm.Headers.Add("", "");
             mm.From = new MailAddress(remitenteCredenciales, "Incom");
             mm.Subject = asunto + " " + utilidad_fechas.obtenerCentral().ToString("f");
             mm.IsBodyHtml = true;
             mm.Body = mensaje;
             mm.Bcc.Add("development@incom.mx, desarrollo@incom.mx");
- 
+
             mm.ReplyToList.Add("cmiranda@it4u.com.mx, rpreza@it4u.com.mx, development@incom.mx");
 
             SmtpClient enviar = smtp();
@@ -119,12 +139,14 @@ public class emailTienda : email {
             resultado = true;
             resultadoMensaje = "Solicitud procesada";
 
-            } else {
+        }
+        else
+        {
             resultado = false;
             resultadoMensaje = "Destinatario(s) no válido(s)";
-            }
-
         }
+
+    }
     public void generalPruebas()
     {
         mm = new MailMessage();
@@ -139,7 +161,7 @@ public class emailTienda : email {
             mm.IsBodyHtml = true;
             mm.Body = mensaje;
             mm.Bcc.Add("development@incom.mx");
- 
+
 
 
             SmtpClient enviar = smtpDevelopment();
@@ -156,12 +178,14 @@ public class emailTienda : email {
         }
 
     }
-    public void restablecerPassword() {
+    public void restablecerPassword()
+    {
 
 
         mm = new MailMessage();
 
-        if (validarDestinatarios()) {
+        if (validarDestinatarios())
+        {
             depurarDestinatarios();
 
             mm.From = new MailAddress(remitenteCredenciales, "Incom Retail");
@@ -175,10 +199,12 @@ public class emailTienda : email {
 
             resultado = true;
             resultadoMensaje = "Solicitud procesada";
-            } else {
+        }
+        else
+        {
             resultado = false;
             resultadoMensaje = "Error el enviar email de restablecimiento";
-            }
-
         }
+
     }
+}
