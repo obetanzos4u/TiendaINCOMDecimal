89fbcb2bd525dd691e1e9577309fc4fec3978f8f
Alexis Ruiz at INCOM <101216883+aruiz-incom@users.noreply.github.com>
2023-01-13 17:11:49 -0600

    CHK agregar productos a cotizaciones

--- userControls/operaciones/uc_modal_agregar_operacion.ascx
+++ userControls/operaciones/uc_modal_agregar_operacion.ascx
@@ -12,20 +12,25 @@
             <ContentTemplate>
                 <asp:HiddenField ID="hf_numero_parte" runat="server" />
                 <asp:HiddenField ID="hf_intIndex_ListViewItemActive" runat="server" />
+                <asp:HiddenField ID="hf_operacion_moneda" runat="server" />
+                <asp:HiddenField ID="hf_tipo_operacion" runat="server" />
                 <div class="row" style="border-left: solid 4px #2196f3;">
                     <div class="col s6 m9 l9 ">
-                        <h1 class="blue-text margin-b-1x margin-t-2x">
+                        <h3 class="blue-text margin-b-1x margin-t-2x">
                             <asp:Label ID="lbl_nombre_operacion" runat="server"></asp:Label>
-                        </h1>
-                        <h2>
-                            <asp:Label ID="lbl_numero_operacion" runat="server"></asp:Label></h2>
-                        Moneda:   <strong>
-                            <asp:Label ID="lbl_moneda" CssClass="blue-text" runat="server"></asp:Label></strong>
-
-                        Nombre:  <strong>
-                            <asp:Label ID="lbl_cliente_nombre" CssClass="blue-text" runat="server"></asp:Label></strong>
-                        <asp:HiddenField ID="hf_operacion_moneda" runat="server" />
-                        <asp:HiddenField ID="hf_tipo_operacion" runat="server" />
+                        </h3>
+                        <p class="is-m-0">
+                            <asp:Label ID="lbl_numero_operacion" runat="server"></asp:Label>
+                        </p>
+                        <p class="is-m-0">
+                            Moneda: <strong>
+                                <asp:Label ID="lbl_moneda" CssClass="blue-text" runat="server"></asp:Label>
+                            </strong>
+                        </p>
+                        <p class="is-m-0">
+                            Nombre: <strong>
+                                <asp:Label ID="lbl_cliente_nombre" CssClass="blue-text" runat="server"></asp:Label></strong>
+                        </p>
                     </div>
                     <div class="col s6 m3 l3 right-align">
                         <asp:LinkButton ID="btn_agregarOperacion" OnClientClick=" $('.mdl_agregarOperacion').modal('close');" OnClick="btn_agregarOperacion_Click" runat="server">Agregar</asp:LinkButton>
@@ -35,35 +40,33 @@
                     <table>
                         <thead>
                             <tr>
-                                <th>No. Parte</th>
-                                <th>Descripción </th>
-                                <th>Cantidad</th>
-                                <th></th>
+                                <th class="is-text-center">No. Parte</th>
+                                <th class="is-text-left">Descripción </th>
+                                <th class="is-text-center">Cantidad</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td style="vertical-align: top;">
-                                    <asp:Label ID="lbl_numero_parte" runat="server"></asp:Label></td>
-                                <td style="vertical-align: top;">
+                                <td class="is-text-center">
+                                    <asp:Label ID="lbl_numero_parte" runat="server"></asp:Label>
+                                </td>
+                                <td class="is-text-left">
                                     <asp:Label ID="lbl_descripcion_corta" runat="server"></asp:Label>
                                 </td>
-                                <td style="vertical-align: top;"></td>
-                                <td>
-                                    <asp:TextBox ID="txt_cantidadCarrito" onchange=" $('.mdl_agregarOperacion').modal('close');" OnTextChanged="txt_cantidadCarrito_TextChanged" AutoPostBack="true" Text="1" runat="server"></asp:TextBox>
+                                <td class="is-text-center is-w-1_4">
+                                    <asp:TextBox ID="txt_cantidadCarrito" onchange=" $('.mdl_agregarOperacion').modal('close');" OnTextChanged="txt_cantidadCarrito_TextChanged" AutoPostBack="true" Text="1" TextMode="Number" Type="Integer" min="1" class="is-w-1_2" runat="server"></asp:TextBox>
                                 </td>
                             </tr>
                         </tbody>
                     </table>
-
                 </div>
                 <div class="row">
-                       <h2 class="blue-text margin-b-1x margin-t-2x">Cotizaciones</h2> Mostrando últimas 15 Cotizaciones creadas:
+                    <h2 class="blue-text margin-b-1x margin-t-2x">Cotizaciones</h2>
+                    <p>Últimas 15 cotizaciones creadas:</p>
                     <asp:ListView ID="lv_cotizaciones" ClientIDMode="AutoID" OnItemDataBound="lv_cotizaciones_OnItemDataBound" runat="server">
                         <LayoutTemplate>
                             <div class="row">
                                 <ul class="collection with-header">
-                                    
                                     <div runat="server" id="itemPlaceholder"></div>
                                 </ul>
                             </div>
@@ -72,22 +75,23 @@
                             <asp:HiddenField ID="hf_indexItem" Value='<%#  Container.DisplayIndex %>' runat="server" />
                             <li id="liItem" class="collection-item" runat="server">
                                 <asp:HiddenField ID="hf_id_cotizacionSQL" Value='<%# Eval("id") %>' runat="server" />
-                                <div style="line-height: 28px;">
-                                    [<asp:Label ID="lbl_cotizacion_moneda" Text='<%# Eval("monedaCotizacion") %>' runat="server"></asp:Label>]
-                                               <strong>
-                                                   <asp:Label ID="lbl_cotizacion_numero_operacion" Text='<%# Eval("numero_operacion") %>' runat="server"></asp:Label></strong>
-                                    -
-                                    <asp:Label ID="lbl_cotizacion_nombre_operacion" Text='<%# Eval("nombre_cotizacion") %>' runat="server"></asp:Label>. | 
-                                  <strong>
-                                      <asp:Label ID="lbl_cotizacion_cliente_nombre" Text='<%# Eval("cliente_nombre") %>' runat="server"></asp:Label>
-                                  </strong> 
-                                    <asp:HyperLink ID="link_cotizacion" Target="_blank" runat="server">Ver</asp:HyperLink>
-                                    <asp:LinkButton ID="btn_seleccionarOperacion"
-                                        OnClick="btn_seleccionarOperacion_Click"
-                                        class="btn btn-s waves-effect waves-light  blue-grey-text text-darken-2 blue-grey lighten-5
-                                                    secondary-content "
-                                        runat="server">
-                                                    Seleccionar</asp:LinkButton>
+                                <div class="is-flex is-justify-between is-items-center">
+                                    <div class="is-flex is-justify-start is-items-center">
+                                        <p class="is-px-4 is-select-all">
+                                            <asp:Label ID="lbl_cotizacion_numero_operacion" Text='<%# Eval("numero_operacion") %>' runat="server"></asp:Label>
+                                        </p>
+                                        <p class="is-px-2">
+                                            <asp:Label ID="lbl_cotizacion_nombre_operacion" Text='<%# Eval("nombre_cotizacion") %>' runat="server"></asp:Label>
+                                        </p>
+                                        <p class="is-px-2">[<asp:Label ID="lbl_cotizacion_moneda" Text='<%# Eval("monedaCotizacion") %>' runat="server"></asp:Label>]</p>
+                                        <p class="is-px-4">
+                                            <asp:Label ID="lbl_cotizacion_cliente_nombre" Text='<%# Eval("cliente_nombre") %>' runat="server"></asp:Label>
+                                        </p>
+                                        <p class="is-px-4">
+                                            <asp:HyperLink ID="link_cotizacion" Target="_blank" runat="server">Ver cotización</asp:HyperLink>
+                                        </p>
+                                    </div>
+                                    <asp:LinkButton ID="btn_seleccionarOperacion" OnClick="btn_seleccionarOperacion_Click" class="btn btn-s waves-effect waves-light blue-grey-text text-darken-2 blue-grey lighten-5 secondary-content" runat="server">Seleccionar</asp:LinkButton>
                                 </div>
                             </li>
                         </ItemTemplate>
@@ -96,27 +100,20 @@
                                 <h2>No tienes cotizaciones activas disponibles.</h2>
                                 <h3>Crea una cotización para agregar productos a ella.</h3>
                                 <label>Nombre de tu cotizacion</label>
-                                <asp:TextBox ID="txt_nombreCotización"   runat="server"></asp:TextBox>
-                               <asp:LinkButton ID="btn_crearCotizacionEnBlanco"  OnClick="btn_crearCotizacionEnBlanco_Click" 
-                                   class="waves-effect waves-light btn blue-grey-text text-darken-2 blue-grey lighten-5 right-align tooltipped"
-                                   data-tooltip="Crear cotización" runat="server">
+                                <asp:TextBox ID="txt_nombreCotización" runat="server"></asp:TextBox>
+                                <asp:LinkButton ID="btn_crearCotizacionEnBlanco" OnClick="btn_crearCotizacionEnBlanco_Click"
+                                    class="waves-effect waves-light btn blue-grey-text text-darken-2 blue-grey lighten-5 right-align tooltipped"
+                                    data-tooltip="Crear cotización" runat="server">
                                      <i class="material-icons right">assignment</i> Crear cotización</a>
-                               </asp:LinkButton>
-                                
-                               
-                                  
+                                </asp:LinkButton>
                             </div>
                         </EmptyDataTemplate>
                     </asp:ListView>
-
                 </div>
             </ContentTemplate>
-
         </asp:UpdatePanel>
     </div>
     <div class="modal-footer">
-        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cerrar ventana</a>
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cerrar</a>
     </div>
-
-
 </div>
--- userControls/operaciones/uc_modal_agregar_operacion.ascx.cs
+++ userControls/operaciones/uc_modal_agregar_operacion.ascx.cs
@@ -9,60 +9,51 @@ using System.Web.UI.WebControls;
 
 public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
 {
-
-    public string numero_parte {
+    public string numero_parte
+    {
         get { return this.hf_numero_parte.Value; }
         set { this.hf_numero_parte.Value = value; }
-        }
-
-
-
+    }
     public string descripcion_corta { get; set; }
-
     private string activeOperation = "collection-item active";
-
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        if (HttpContext.Current.User.Identity.IsAuthenticated) {
+        if (HttpContext.Current.User.Identity.IsAuthenticated)
+        {
             mdl_agregarOperacion.Visible = true;
-            }
-        
-        if (!IsPostBack) {
+        }
 
-  
-            }
-  
+        if (!IsPostBack)
+        {
+
+
         }
-    private void modalDOMshow() {
+    }
+    private void modalDOMshow()
+    {
         ScriptManager.RegisterStartupScript(this.Page, typeof(Control), "mdl_agregarOperacion", "$(document).ready(function() {  M.AutoInit();   $('.mdl_agregarOperacion').modal('open');  });", true);
-        }
-    public void mostrarModal() {
-     
-      
+    }
+    public void mostrarModal()
+    {
         lbl_numero_parte.Text = numero_parte;
         lbl_descripcion_corta.Text = descripcion_corta;
         loadCotizaciones();
         modalDOMshow();
-        }
-   
-    protected void loadCotizaciones() {
-
+    }
+    protected void loadCotizaciones()
+    {
         cotizaciones obtener = new cotizaciones();
         usuarios datosUsuario = usuarios.modoAsesor();
-
-        
         DataTable dtCotizaciones = obtener.obtenerCotizacionesUsuario_min(datosUsuario.email, 15, " AND datos.activo = 1 AND datos.conversionPedido = 0 ");
 
         //Agregamos la columna que nos sirve para poner la css class como activa
         dtCotizaciones.Columns.Add("active", typeof(System.String));
 
-
         // Estas variables se crean al dar click editar la operacion  para posicionarla en primer lugar la operación deseada a editar.
-        if (Session["cotizacion_edit"] != null && Session["cotizacion_edit_idSQL"] != null) {
-
+        if (Session["cotizacion_edit"] != null && Session["cotizacion_edit_idSQL"] != null)
+        {
             hf_tipo_operacion.Value = "cotizacion";
-
             hf_intIndex_ListViewItemActive.Value = "0";
             // Obteneos el DT con la operación y también agregamos la columna para el CSS class
             DataTable dtCotizacionEdit = obtener.obtenerCotizacionDatos_min(int.Parse(Session["cotizacion_edit_idSQL"].ToString()));
@@ -71,9 +62,7 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
             // Al ser un único valor que traeremos le añadimos el texto con las propiedades del estilo
             dtCotizacionEdit.Rows[0]["active"] = activeOperation;
 
-          
-
-            lbl_numero_operacion.Text = dtCotizacionEdit.Rows[0]["numero_operacion"].ToString() ;
+            lbl_numero_operacion.Text = "Operación: " + dtCotizacionEdit.Rows[0]["numero_operacion"].ToString();
             lbl_nombre_operacion.Text = dtCotizacionEdit.Rows[0]["nombre_cotizacion"].ToString();
             lbl_cliente_nombre.Text = dtCotizacionEdit.Rows[0]["cliente_nombre"].ToString();
             lbl_moneda.Text = dtCotizacionEdit.Rows[0]["monedaCotizacion"].ToString();
@@ -89,28 +78,25 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
 
             btn_agregarOperacion.Enabled = true;
             btn_agregarOperacion.CssClass = " btn blue";
-            } 
-        else {
-            lbl_nombre_operacion.Text = "Selecciona una cotización";
-            lbl_numero_operacion.Text = "---"; 
+        }
+        else
+        {
+            lbl_nombre_operacion.Text = "Cotización";
+            lbl_numero_operacion.Text = "Número de operación: ---";
             lbl_moneda.Text = "---";
             lbl_cliente_nombre.Text = "---";
-
             btn_agregarOperacion.CssClass = " btn disabled";
             btn_agregarOperacion.Enabled = false;
-
-
-            }
-
+        }
 
         lv_cotizaciones.DataSource = dtCotizaciones;
         lv_cotizaciones.DataBind();
 
         up_operacionesAdd.Update();
-     
-        }
+    }
 
-    protected void lv_cotizaciones_OnItemDataBound(object sender, ListViewItemEventArgs e) {
+    protected void lv_cotizaciones_OnItemDataBound(object sender, ListViewItemEventArgs e)
+    {
 
         System.Data.DataRowView rowView = e.Item.DataItem as System.Data.DataRowView;
         HtmlGenericControl liItem = (HtmlGenericControl)e.Item.FindControl("liItem");
@@ -122,18 +108,20 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
         HiddenField hf_id_cotizacionSQL = (HiddenField)liItem.FindControl("hf_id_cotizacionSQL");
         HyperLink link_cotizacion = (HyperLink)liItem.FindControl("link_cotizacion");
 
-        if (hf_indexItem.Value == hf_intIndex_ListViewItemActive.Value) {
-           liItem.Attributes.Add("class", "collection-item active");
+        if (hf_indexItem.Value == hf_intIndex_ListViewItemActive.Value)
+        {
+            liItem.Attributes.Add("class", "collection-item active");
             link_cotizacion.Attributes.Add("class", "white-text");
 
-            if ( lbl_numero_operacion.Text == "Selecciona una operación"){
+            if (lbl_numero_operacion.Text == "Selecciona una operación")
+            {
 
-             
+
                 Label lbl_cotizacion_numero_operacion = (Label)liItem.FindControl("lbl_cotizacion_numero_operacion");
                 Label lbl_cotizacion_nombre_operacion = (Label)liItem.FindControl("lbl_cotizacion_nombre_operacion");
                 Label lbl_cotizacion_cliente_nombre = (Label)liItem.FindControl("lbl_cotizacion_cliente_nombre");
                 Label lbl_cotizacion_moneda = (Label)liItem.FindControl("lbl_cotizacion_moneda");
-             
+
 
 
                 string numero_operacion = lbl_cotizacion_numero_operacion.Text;
@@ -143,7 +131,7 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
 
 
 
-            
+
 
                 lbl_numero_operacion.Text = numero_operacion;
                 lbl_nombre_operacion.Text = nombre_operacion;
@@ -152,72 +140,78 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
 
                 btn_agregarOperacion.Enabled = true;
                 btn_agregarOperacion.CssClass = " btn blue";
-                }
-            } else liItem.Attributes.Add("class", "collection-item ");
+            }
+        }
+        else liItem.Attributes.Add("class", "collection-item ");
 
         link_cotizacion.NavigateUrl = GetRouteUrl("usuario-cotizacion-visualizar", new System.Web.Routing.RouteValueDictionary {
                         { "id_operacion",seguridad.Encriptar(hf_id_cotizacionSQL.Value) }
                     });
         preLoad();
 
-        }
+    }
 
-         protected void preLoad() {
+    protected void preLoad()
+    {
 
-     //   if (!string.IsNullOrEmpty(hf_intIndex_ListViewItemActive.Value)) { 
-       // int.Parse(hf_intIndex_ListViewItemActive.Value);
+        //   if (!string.IsNullOrEmpty(hf_intIndex_ListViewItemActive.Value)) { 
+        // int.Parse(hf_intIndex_ListViewItemActive.Value);
 
-            foreach (ListViewItem item in lv_cotizaciones.Items) {
+        foreach (ListViewItem item in lv_cotizaciones.Items)
+        {
 
-                Label lbl_cotizacion_numero_operacion = (Label)item.FindControl("lbl_cotizacion_numero_operacion");
-         
+            Label lbl_cotizacion_numero_operacion = (Label)item.FindControl("lbl_cotizacion_numero_operacion");
 
-            if (lbl_cotizacion_numero_operacion.Text == lbl_numero_operacion.Text) {
+
+            if (lbl_cotizacion_numero_operacion.Text == lbl_numero_operacion.Text)
+            {
 
                 HyperLink link_cotizacion = (HyperLink)item.FindControl("link_cotizacion");
                 link_cotizacion.Attributes.Add("class", "white-text");
 
                 Label lbl_cotizacion_nombre_operacion = (Label)item.FindControl("lbl_cotizacion_nombre_operacion");
-                    Label lbl_cotizacion_cliente_nombre = (Label)item.FindControl("lbl_cotizacion_cliente_nombre");
-                    Label lbl_cotizacion_moneda = (Label)item.FindControl("lbl_cotizacion_moneda");
+                Label lbl_cotizacion_cliente_nombre = (Label)item.FindControl("lbl_cotizacion_cliente_nombre");
+                Label lbl_cotizacion_moneda = (Label)item.FindControl("lbl_cotizacion_moneda");
 
-                    string numero_operacion = lbl_cotizacion_numero_operacion.Text;
-                    string nombre_operacion = lbl_cotizacion_nombre_operacion.Text;
-                    string cliente_nombre = lbl_cotizacion_cliente_nombre.Text;
-                    string moneda = lbl_cotizacion_moneda.Text;
+                string numero_operacion = lbl_cotizacion_numero_operacion.Text;
+                string nombre_operacion = lbl_cotizacion_nombre_operacion.Text;
+                string cliente_nombre = lbl_cotizacion_cliente_nombre.Text;
+                string moneda = lbl_cotizacion_moneda.Text;
 
-                    lbl_numero_operacion.Text = numero_operacion;
-                    lbl_nombre_operacion.Text = nombre_operacion;
-                    lbl_cliente_nombre.Text = cliente_nombre;
-                    lbl_moneda.Text = moneda;
+                lbl_numero_operacion.Text = numero_operacion;
+                lbl_nombre_operacion.Text = nombre_operacion;
+                lbl_cliente_nombre.Text = cliente_nombre;
+                lbl_moneda.Text = moneda;
 
-                    btn_agregarOperacion.Enabled = true;
-                    btn_agregarOperacion.CssClass = " btn blue";
-                    }
-                }
-
-         //   }
+                btn_agregarOperacion.Enabled = true;
+                btn_agregarOperacion.CssClass = " btn blue";
+            }
         }
 
-        protected void btn_seleccionarOperacion_Click(object sender, EventArgs e) {
+        //   }
+    }
+
+    protected void btn_seleccionarOperacion_Click(object sender, EventArgs e)
+    {
         hf_tipo_operacion.Value = "cotizacion";
         // Obtenemos el elemento que activo el click
         LinkButton btn_seleccionarOperacion = sender as LinkButton;
 
         // Obtenemos el index del elemento activo
-        if (hf_intIndex_ListViewItemActive.Value != "") {
+        if (hf_intIndex_ListViewItemActive.Value != "")
+        {
             HtmlGenericControl old_itemActive = (HtmlGenericControl)lv_cotizaciones.Items[int.Parse(hf_intIndex_ListViewItemActive.Value)].FindControl("liItem");
             // Removemos el estilo
             old_itemActive.Attributes.Add("class", "collection-item");
 
-            lbl_nombre_operacion.Text = "Selecciona una operación"; 
-           lbl_numero_operacion.Text = "---";
+            lbl_nombre_operacion.Text = "Selecciona una operación";
+            lbl_numero_operacion.Text = "---";
             lbl_moneda.Text = "---";
             lbl_cliente_nombre.Text = "---";
 
             btn_agregarOperacion.CssClass = " btn disabled";
             btn_agregarOperacion.Enabled = false;
-            }
+        }
         btn_agregarOperacion.Enabled = true;
         btn_agregarOperacion.CssClass = " btn blue";
         // Obtenemos el contenedor, en este caso no es un ListView Item si no un HTML Generic
@@ -236,9 +230,9 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
 
         // Cambiamos el viejo valor por el nuemo
         hf_intIndex_ListViewItemActive.Value = hf_indexItem.Value;
-        
 
-       
+
+
 
         Label lbl_cotizacion_numero_operacion = (Label)item.FindControl("lbl_cotizacion_numero_operacion");
         Label lbl_cotizacion_nombre_operacion = (Label)item.FindControl("lbl_cotizacion_nombre_operacion");
@@ -249,7 +243,7 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
         string nombre_operacion = lbl_cotizacion_nombre_operacion.Text;
         string cliente_nombre = lbl_cotizacion_cliente_nombre.Text;
         string moneda = lbl_cotizacion_moneda.Text;
-        
+
         lbl_numero_operacion.Text = numero_operacion;
         lbl_nombre_operacion.Text = nombre_operacion;
         lbl_cliente_nombre.Text = cliente_nombre;
@@ -258,45 +252,50 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
 
         modalDOMshow();
 
-        }
+    }
     protected void txt_cantidadCarrito_TextChanged(object sender, EventArgs e)
     {
         TextBox txt_cantidad = sender as TextBox;
 
-        if(txt_cantidad.Text != "" && txt_cantidad.Text != null)
+        if (txt_cantidad.Text != "" && txt_cantidad.Text != null)
         {
             double cantidad = 1;
-            try { 
+            try
+            {
                 cantidad = textTools.soloNumeros(txt_cantidad.Text);
             }
-            catch (Exception ex) {
-           //     materializeCSS.crear_toast(this.Page, "Debe ingresar valores númericos", false);
+            catch (Exception ex)
+            {
+                //     materializeCSS.crear_toast(this.Page, "Debe ingresar valores númericos", false);
                 txt_cantidad.Text = "1";
             }
-            
+
 
             if (cantidad < 1) { txt_cantidad.Text = "1"; }
         }
 
         modalDOMshow();
 
-        }
+    }
 
 
 
-    protected async void btn_agregarOperacion_Click(object sender, EventArgs e) {
+    protected async void btn_agregarOperacion_Click(object sender, EventArgs e)
+    {
 
         string tipo_operacion = hf_tipo_operacion.Value;
         string numero_operacion = lbl_numero_operacion.Text;
-        operacionesProductos agregar = new operacionesProductos(tipo_operacion,"", numero_operacion, lbl_numero_parte.Text, txt_cantidadCarrito.Text, lbl_moneda.Text);
+        operacionesProductos agregar = new operacionesProductos(tipo_operacion, "", numero_operacion, lbl_numero_parte.Text, txt_cantidadCarrito.Text, lbl_moneda.Text);
         await agregar.agregarProductoAsync();
 
         materializeCSS.crear_toast(this.Page, agregar.mensaje_ResultadoOperacion, agregar.resultado_operacion);
 
         // S egregó correctamente a la cotización, realizamos cálculo de flete
-        if (agregar.resultado_operacion == true) {
+        if (agregar.resultado_operacion == true)
+        {
 
-            try{
+            try
+            {
                 ValidarCalculoEnvioOperacion validar = new ValidarCalculoEnvioOperacion(numero_operacion, "cotizacion");
                 materializeCSS.crear_toast(this, validar.Message, validar.OperacionValida);
 
@@ -312,7 +311,7 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
         modalDOMshow();
 
 
-        }
+    }
 
     protected async void btn_crearCotizacionEnBlanco_Click(object sender, EventArgs e)
     {
@@ -320,7 +319,7 @@ public partial class uc_modal_agregar_operacion : System.Web.UI.UserControl
         LinkButton btn = (LinkButton)sender;
         TextBox txt_nombreCotización = lv_cotizaciones.Controls[0].FindControl("txt_nombreCotización") as TextBox;
 
-        string  nombreCotización = textTools.lineSimple( txt_nombreCotización.Text);
+        string nombreCotización = textTools.lineSimple(txt_nombreCotización.Text);
         usuarios datosUsuario = usuarios.modoAsesor();
         var result = await CotizacionesEF.CrearCotizacionEnBlanco(datosUsuario, nombreCotización, "MXN");
 
